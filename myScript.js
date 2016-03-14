a2c81d9255fd809a9821d3c490700459907dc8cd
zznapnap <zznapnap@gmail.com>
2016-03-14 22:19:15 +0800

    static web page

---
+++ myScript.js
@@ -0,0 +1,18 @@
+function destinationOver(){
+document.getElementById("destination-boxborder").style.display = "block";
+}
+function destinationOut(){
+document.getElementById("destination-boxborder").style.display = "none";
+}
+function commentOver(){
+document.getElementById("comment-box").style.display = "block";
+}
+function commentOut(){
+document.getElementById("comment-box").style.display = "none";
+}
+function picOver(){
+document.getElementById("picture-box").style.display = "block";
+}
+function picOut(){
+document.getElementById("picture-box").style.display = "none";
+}
