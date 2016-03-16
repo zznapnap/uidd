7f999fbd80ae5db0bb153bffc1bc97c07edfe667
zznapnap <zznapnap@gmail.com>
2016-03-16 17:20:40 +0800

    top-button animation

--- myScript.js
+++ myScript.js
@@ -1,18 +1,30 @@
 function destinationOver(){
-document.getElementById("destination-boxborder").style.display = "block";
+    document.getElementById("destination-boxborder").style.display = "block";
 }
 function destinationOut(){
-document.getElementById("destination-boxborder").style.display = "none";
+    document.getElementById("destination-boxborder").style.display = "none";
 }
 function commentOver(){
-document.getElementById("comment-box").style.display = "block";
+    document.getElementById("comment-box").style.display = "block";
 }
 function commentOut(){
-document.getElementById("comment-box").style.display = "none";
+    document.getElementById("comment-box").style.display = "none";
 }
 function picOver(){
-document.getElementById("picture-box").style.display = "block";
+    document.getElementById("picture-box").style.display = "block";
 }
 function picOut(){
-document.getElementById("picture-box").style.display = "none";
+    document.getElementById("picture-box").style.display = "none";
 }
+var topbutton=1;
+$( ".div_button" ).hover(
+        function() {
+        if(topbutton==1){
+        $( this ).animate({ backgroundColor: "#748787" },1000);
+        setTimeout(function(){$( ".top-button" ).slideDown("slow");},1000);
+        setTimeout(function(){$(".div_button").animate({ backgroundColor: "rgba(255, 255, 255, 0)" });},2000);
+        topbutton=0;
+        }
+        }, function() {
+        }
+);
